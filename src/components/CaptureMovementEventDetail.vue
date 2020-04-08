b19793ecdd394fc3b54dc33c94b85b540dd123e0
Lester Jan Artienda <artiele@oocl.com>
2020-04-09 06:57:33 +0800

    Complete Initial Draft on CaptureMovementEventHeader

---
+++ src/components/CaptureMovementEventDetail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <form action>CaptureMovementEventDetail</form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CaptureMovementEventDetail",
+  data() {
+    return {};
+  }
+};
+</script>
---
+++ src/components/TitleBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="title">{{ title }}</div>
+</template>
+<script>
+export default {
+  name: "TitleBar",
+  data() {
+    return {
+      title: "Smart Vendor Platform"
+    };
+  }
+};
+</script>
+<style scoped>
+.title {
+  padding: 10px;
+}
+</style>
