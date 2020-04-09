66de998829e9a0be7563df936c6d7041f1e18938
Lester Jan Artienda <artiele@oocl.com>
2020-04-09 23:16:12 +0800

    First draft CaptureMovementEventDetailItem

--- src/components/CaptureMovementEventDetail.vue
+++ src/components/CaptureMovementEventDetail.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    <form action>CaptureMovementEventDetail</form>
+    <v-row>
+      <v-col>
+        <v-btn class="mr-2" :elevation="7" tile outlined color="info">Add Rows</v-btn>
+        <v-btn class="mr-2" :elevation="7" tile outlined color="success">Edit Rows</v-btn>
+        <v-btn class="mr-2" :elevation="7" tile outlined color="warning">Remove Rows</v-btn>
+        <v-btn class="mr-2" :elevation="7" tile outlined color="error">Reset Rows</v-btn>
+        <v-btn :elevation="7" tile outlined color="blue-grey darken-1">Toggle Column Display</v-btn>
+        <!-- open dropdown to de-select column display -->
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <CaptureMovementEventDetailItem />
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
+import CaptureMovementEventDetailItem from "./CaptureMovementEventDetailItem";
+
 export default {
   name: "CaptureMovementEventDetail",
+  components: {
+    CaptureMovementEventDetailItem
+  },
   data() {
     return {};
   }
