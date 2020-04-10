cd8a2f1977d779925ee3a3fe6b74473b903cd322
Lester Jan Artienda <artiele@oocl.com>
2020-04-10 20:05:45 +0800

    Establish one-way binding of common values with input fields

--- src/components/CaptureMovementEventDetailItem.vue
+++ src/components/CaptureMovementEventDetailItem.vue
@@ -1,25 +1,6 @@
 <template>
-  <!-- <v-data-table
-    v-model="selected"
-    :headers="headers"
-    :items="events"
-    :single-select="singleSelect"
-    item-key="name"
-    show-select
-    class="elevation-1"
-  >
-    <template v-slot:top>
-      <v-switch
-        v-model="singleSelect"
-        label="Single select"
-        class="pa-3"
-      ></v-switch>
-    </template>
-  </v-data-table> -->
-
   <div>
-    <v-simple-table :dense="true" :fixed-header="true">
-      <!-- <template v-slot:events> -->
+    <table>
       <thead>
         <tr>
           <th>Customer</th>
@@ -40,26 +21,45 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="event in events" :key="event">
-          <td>{{ commonValues.customer }}</td>
-          <td>{{ commonValues.eqpType }}</td>
-          <td>{{ commonValues.authNo }}</td>
-          <td>{{ commonValues.vgmMethod }}</td>
-          <td>{{ commonValues.eventDate }}</td>
-          <td>{{ commonValues.condition }}</td>
-          <td>{{ commonValues.authType }}</td>
-          <td>{{ commonValues.eventType }}</td>
-          <td>{{ commonValues.vgmDate }}</td>
-          <td>{{ commonValues.carrier }}</td>
-          <td>{{ commonValues.facility }}</td>
-          <td>{{ commonValues.vgmResParty }}</td>
-          <td>{{ commonValues.ladenEmpty }}</td>
-          <td>{{ commonValues.sealType }}</td>
-          <td>{{ commonValues.vgmOfficial }}</td>
+        <tr
+          v-for="({
+            e_customer = commonValues.customer,
+            e_eqpType = commonValues.eqpType,
+            e_authNo = commonValues.authNo,
+            e_vgmMethod = commonValues.vgmMethod,
+            e_eventDate = commonValues.eventDate,
+            e_condition = commonValues.condition,
+            e_authType = commonValues.authType,
+            e_eventType = commonValues.eventType,
+            e_vgmDate = commonValues.vgmDate,
+            e_carrier = commonValues.carrier,
+            e_facility = commonValues.facility,
+            e_vgmResParty = commonValues.vgmResParty,
+            e_ladenEmpty = commonValues.ladenEmpty,
+            e_sealType = commonValues.sealType,
+            e_vgmOfficial = commonValues.vgmOfficial,
+          },
+          i) in events"
+          :key="i"
+        >
+          <td><input type="text" :value="e_customer" /></td>
+          <td><input type="text" :value="e_eqpType" /></td>
+          <td><input type="text" :value="e_authNo" /></td>
+          <td><input type="text" :value="e_vgmMethod" /></td>
+          <td><input type="text" :value="e_eventDate" /></td>
+          <td><input type="text" :value="e_condition" /></td>
+          <td><input type="text" :value="e_authType" /></td>
+          <td><input type="text" :value="e_eventType" /></td>
+          <td><input type="text" :value="e_vgmDate" /></td>
+          <td><input type="text" :value="e_carrier" /></td>
+          <td><input type="text" :value="e_facility" /></td>
+          <td><input type="text" :value="e_vgmResParty" /></td>
+          <td><input type="text" :value="e_ladenEmpty" /></td>
+          <td><input type="text" :value="e_sealType" /></td>
+          <td><input type="text" :value="e_vgmOfficial" /></td>
         </tr>
       </tbody>
-      <!-- </template> -->
-    </v-simple-table>
+    </table>
   </div>
 </template>
 
@@ -73,60 +73,25 @@ export default {
   },
   data() {
     return {
-      events: new Array(10),
-      // customer: "",
-      // eqpType: "",
-      // authNo: "",
-      // vgmMethod: "",
-      // eventDate: "",
-      // condition: "",
-      // authType: "",
-      // eventType: "",
-      // vgmDate: "",
-      // carrier: "",
-      // facility: "",
-      // vgmResParty: "",
-      // ladenEmpty: "",
-      // sealType: "",
-      // vgmOfficial: "",
-      // singleSelect: false,
-      // selected: [],
-      // headers: [
-      //   { text: "Customer", value: "customer" },
-      //   { text: "Equipment Type", value: "eqpType" },
-      //   { text: "Authorization Number", value: "authNo" },
-      //   { text: "VGM Method", value: "vgmMethod" },
-      //   { text: "Event Date", value: "eventDate" },
-      //   { text: "Equipment Condition", value: "condition" },
-      //   { text: "Authorization Type", value: "authType" },
-      //   { text: "Event Type", value: "eventType" },
-      //   { text: "VGM Date", value: "vgmDate" },
-      //   { text: "Carrier", value: "carrier" },
-      //   { text: "VGM Responsible Party", value: "facility" },
-      //   { text: "Facility", value: "vgmResParty" },
-      //   { text: "Equipment Type", value: "ladenEmpty" },
-      //   { text: "Seal Type", value: "sealType" },
-      //   { text: "VGM Authorized Official", value: "vgmOfficial" },
-      // ],
-      // events: [
-      //   {
-      //     customer: "customer",
-      //     eqpType: "TEST",
-      //     authNo: "TEST",
-      //     vgmMethod: "TEST",
-      //     eventDate: "TEST",
-      //     condition: "TEST",
-      //     authType: "TEST",
-      //     eventType: "TEST",
-      //     vgmDate: "TEST",
-      //     carrier: "TEST",
-      //     facility: "TEST",
-      //     vgmResParty: "TEST",
-      //     ladenEmpty: "TEST",
-      //     sealType: "TEST",
-      //     vgmOfficial: "TEST",
-      //   },
-      // ],
+      events: [
+        {
+          customer: "",
+          eqpType: "",
+          authNo: "",
+          vgmMethod: "",
+          eventDate: "",
+          condition: "",
+          authType: "",
+          eventType: "",
+          vgmDate: "",
+          carrier: "",
+          facility: "",
+          vgmResParty: "",
+          ladenEmpty: "",
+          sealType: "",
+          vgmOfficial: "",
+        },
+      ],
     };
   },
 };
